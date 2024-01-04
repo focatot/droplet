5190f40f35bae40ae2e1985832d1b7f137b97788
focatot <farias.jonathan@icloud.com>
2024-01-04 21:21:51 +0100

    added short descriptions for min and max, added weather.description element to be displayed

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ function getInputField() {
 function getWeatherElements() {
   return {
   city: document.getElementById('cityName'),
+  description: document.getElementById('descriptor'),
   temp: document.getElementById('currentTemp'),
   feels: document.getElementById('feelsLike'),
   humid: document.getElementById('currentHumid'),
@@ -119,11 +120,12 @@ function updateWeatherInfo(weatherData) {
   inputField.value = "";
   const weatherElements = getWeatherElements();
   weatherElements.city.innerHTML = `<h1>${weatherData.name}, ${weatherData.sys.country}</h1>`;
+  weatherElements.descriptor.innerHTML = `<p>${weatherData.description} &deg;C<p>`;
   weatherElements.temp.innerHTML = `<p>${weatherData.main.temp} &deg;C<p>`;
   weatherElements.feels.innerHTML = `<p>Feels Like:</br>${weatherData.main.feels_like} &deg;C</p>`;
   weatherElements.humid.innerHTML = `<p>Humidity:</br>${weatherData.main.humidity}%</p>`;
-  weatherElements.min.innerHTML = `<p>${weatherData.main.temp_min} &deg;C</p>`;
-  weatherElements.max.innerHTML = `<p>${weatherData.main.temp_max} &deg;C</p>`;
+  weatherElements.min.innerHTML = `<p>L:${weatherData.main.temp_min} &deg;C</p>`;
+  weatherElements.max.innerHTML = `<p>H:${weatherData.main.temp_max} &deg;C</p>`;
   weatherElements.press.innerHTML = `<p>Pressure:</br>${weatherData.main.pressure} hPa</p>`;
   weatherElements.vision.innerHTML = `<p>Visibility:</br>${weatherData.visibility / 1000} km</p>`;
   weatherElements.wind.innerHTML = `<p>Wind Speed:</br>${(weatherData.wind.speed * 1.60934).toFixed(0)} km/h</p>`;
