ddef7801fbe0223bbc41a60e140d047839c7ba9f
focatot <farias.jonathan@icloud.com>
2024-02-13 21:20:39 +0100

    code cleanup

--- mapbox-script.js
+++ mapbox-script.js
@@ -6,41 +6,34 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZmNheWF5byIsImEiOiJjbHJ5MmR3bjgxZWp4MmpwYndpe
 // OpenWM key
 openwmAccessToken = '4c2ea446b8fba1b6f13c58bda72e19b2';
 
-// Init map 
-const map = new mapboxgl.Map({
+const map = new mapboxgl.Map({ // Init map 
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/dark-v11", // dark mode :pp 
     center: [-66.915660, 10.505404], // starting position [lng, lat] NEEDS INTEGRATION TO USER LCATION
     zoom: 10, // raise to zoom in 
 });
 
-// Init geocoder 
-var geocoder = new MapboxGeocoder({
+var geocoder = new MapboxGeocoder({ // Init geocoder 
     accessToken: mapboxgl.accessToken,
     mapboxgl: mapboxgl,
     marker: false,
     types: 'place' // Restrict search 
 });
 
-// Init search box control 
-map.addControl(geocoder);
+map.addControl(geocoder); // Init search box control 
 
-// Add a click event listener to the map
-map.on('click', function(e) {
+map.on('click', function(e) { // Add a click event listener to the map
 
     const clickedCoordinates = e.lngLat; // Extract the clicked coordinates
     const longitude = clickedCoordinates.lng; // Store lng
     const latitude = clickedCoordinates.lat; // Store lat
 
-    // Reverse geocode the clicked coordinates to retrieve the city name
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxgl.accessToken}`)
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxgl.accessToken}`) // Reverse geocode the clicked coordinates to retrieve the city name
         .then(response => response.json())
         .then(data => {
-            // Extract the place name from the API response
-            const city = data.features[0].place_name;
+            const city = data.features[0].place_name; // Extract the place name from the API response (NEEDS ERROR HANDELING FOR NONSEARCHABLE LOCATIONS LIKE OCEAN AND SUCH)
 
-            // Fetch weather data for the clicked coordinates
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openwmAccessToken}&units=metric`)
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openwmAccessToken}&units=metric`) // Fetch weather data for the clicked coordinates
                 .then(response => response.json())
                 .then(weatherData => {
                     console.log("weather fetched from globe click for", city);
@@ -57,16 +50,11 @@ map.on('click', function(e) {
         });
 });
 
+geocoder.on('result', function(e) { // Listen for the 'result' event and init weather request
 
-
-// Listen for the 'result' event and init weather request
-geocoder.on('result', function(e) {
-
-    // Extract the selected place name from the event object
-    const selectedPlaceName = e.result.place_name;
+    const selectedPlaceName = e.result.place_name;// Extract the selected place name from the event object
     
-    // Fetch data from the Mapbox API using the selected place name
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${selectedPlaceName}.json?access_token=${mapboxgl.accessToken}`)
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${selectedPlaceName}.json?access_token=${mapboxgl.accessToken}`)  // Fetch data from the Mapbox API using the selected place name
         .then(response => response.json())
         .then(data => {
             console.log("fetching location..."); // Extract the city name from the API response
@@ -75,8 +63,7 @@ geocoder.on('result', function(e) {
             console.log("location fetched for", city);
             console.log(longitude, latitude);
             console.log("fetching weather...");
-            // Fetch data from the OpenWM API using the selected coordinates
-            return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openwmAccessToken}&units=metric`)
+            return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openwmAccessToken}&units=metric`) // Fetch data from the OpenWM API using the selected coordinates
                 .then(response => response.json())
                 .then(weatherData => {
                     console.log("weather fetched for", city);
@@ -89,8 +76,7 @@ geocoder.on('result', function(e) {
         })
 });
 
-// Init weather request 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=caracas&appid=4c2ea446b8fba1b6f13c58bda72e19b2&units=metric`) // Based in default map center coordinates "caracas"
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=caracas&appid=4c2ea446b8fba1b6f13c58bda72e19b2&units=metric`) // Init weather request 
     .then(response => response.json())
     .then(weatherData => {
         const city = "Caracas, Capital District, Venezuela"; // Set the city manually since it's not extracted from geocoder in this fetch
@@ -98,8 +84,7 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=caracas&appid=4c2ea446b
     })
     .catch(error => console.log('error', error));
 
-// Update dashboard with retrieved values
-function updateWeatherInfo(weatherData, city, country) {
+function updateWeatherInfo(weatherData, city, country) { // Update dashboard with retrieved values and assign values to HTML elements
     const cityNameElement = document.getElementById('cityName');
     const descriptionElement = document.getElementById('descriptor');
     const tempElement = document.getElementById('currentTemp');
@@ -114,7 +99,6 @@ function updateWeatherInfo(weatherData, city, country) {
     const riseElement = document.getElementById('sunrise');
     const setElement = document.getElementById('sunset');
 
-    // Assign values to HTML elements
     cityNameElement.innerHTML = `<p>${city}</p>`;
     descriptionElement.innerHTML = `<p>${weatherData.weather[0].description}</p>`;
     tempElement.innerHTML = `<p>${weatherData.main.temp.toFixed(0)}&deg;</p>`;
