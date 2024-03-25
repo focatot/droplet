e8ab0e04120611d76572f16ff8bb2c13213fa09d
focatot <farias.jonathan@icloud.com>
2024-03-25 04:17:34 +0100

    minor styling, new value placement, new default font for consistency, right grid reduced in size. grid needs work

--- mapbox-script.js
+++ mapbox-script.js
@@ -1,22 +1,25 @@
 // mapbox-script.js
 
-// Mapbox key
+// MAPBOX KEY
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmNheWF5byIsImEiOiJjbHJ5MmR3bjgxZWp4MmpwYndpejRzc2pqIn0.WnTM1dHJuY92ek7FPXHE_w';
 
-// OpenWM key
+// OPENWM KEY
 openwmAccessToken = '4c2ea446b8fba1b6f13c58bda72e19b2';
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const map = new mapboxgl.Map({ // Init map 
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/dark-v11", // dark mode :pp 
-    center: [-66.91548939287081, 10.505832123619967], // starting position [lng, lat] NEEDS INTEGRATION TO USER LCATION
+    center: [139.7263785, 35.6652065], // starting position [lng, lat] NEEDS INTEGRATION TO USER LCATION
     zoom: 10, // raise to zoom in 
     // attributionControl: false,
 });
 
-map.on('load', function() { 
+map.on('load', function () {
     map.resize(); // Dinamically rezises and centers map
-    });
+});
 
 var geocoder = new MapboxGeocoder({ // Init geocoder 
     accessToken: mapboxgl.accessToken,
@@ -30,10 +33,10 @@ document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
 // map.addControl(geocoder); // Init search box control 
 
-geocoder.on('result', function(e) { // Listen for the 'result' event and init weather request
+geocoder.on('result', function (e) { // Listen for the 'result' event and init weather request
 
     const selectedPlaceName = e.result.place_name; // Extract the selected place name from the event object
-    
+
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${selectedPlaceName}.json?access_token=${mapboxgl.accessToken}`)  // Fetch data from the Mapbox API using the selected place name
         .then(response => response.json())
         .then(data => {
@@ -55,7 +58,7 @@ geocoder.on('result', function(e) { // Listen for the 'result' event and init we
         })
 });
 
-map.on('click', function(e) { // Add a click event listener to the map
+map.on('click', function (e) { // Add a click event listener to the map
 
     const clickedCoordinates = e.lngLat; // Extract the clicked coordinates
     const longitude = clickedCoordinates.lng; // Store lng
@@ -101,10 +104,10 @@ function extractCountry(data) { // Extract country name from the response
     return context[context.length - 1].text; // Assumes the country name is the last element in the context array
 }
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=caracas&appid=4c2ea446b8fba1b6f13c58bda72e19b2&units=metric`) // Init weather request 
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=4c2ea446b8fba1b6f13c58bda72e19b2&units=metric`) // Init weather request 
     .then(response => response.json())
     .then(weatherData => {
-        const city = "Caracas, Capital District, Venezuela"; // Set the city manually since it's not extracted from geocoder in this fetch
+        const city = "Tokyo, Japan"; // Set the city manually since it's not extracted from geocoder in this fetch
         updateWeatherInfo(weatherData, city);
     })
     .catch(error => console.log('error', error));
@@ -117,11 +120,9 @@ function updateWeatherInfo(weatherData, cityName) { // Update dashboard with ret
     const humidElement = document.getElementById('currentHumid');
     const minElement = document.getElementById('minTemp');
     const maxElement = document.getElementById('maxTemp');
-    const visionElement = document.getElementById('visibility');
     const windElement = document.getElementById('windSpd');
     const cloudElement = document.getElementById('cloudiness');
-    const riseElement = document.getElementById('sunrise');
-    const setElement = document.getElementById('sunset');
+    const risesetElement = document.getElementById('risenset');
 
     cityNameElement.innerHTML = `<p>${cityName}</p>`;
     descriptionElement.innerHTML = `<p>${weatherData.weather[0].description}</p>`;
@@ -130,9 +131,8 @@ function updateWeatherInfo(weatherData, cityName) { // Update dashboard with ret
     humidElement.innerHTML = `<p>Humidity:</br>${weatherData.main.humidity}%</p>`;
     minElement.innerHTML = `<p>L: ${weatherData.main.temp_min.toFixed(0)}&deg;</p>`;
     maxElement.innerHTML = `<p>H: ${weatherData.main.temp_max.toFixed(0)}&deg;</p>`;
-    visionElement.innerHTML = `<p>Visibility:</br>${(weatherData.visibility / 1000).toFixed(1)} km</p>`;
     windElement.innerHTML = `<p>Wind Speed:</br>${(weatherData.wind.speed * 1.60934).toFixed(0)} km/h</p>`;
     cloudElement.innerHTML = `<p>Cloudiness:</br>${weatherData.clouds.all}%</p>`;
-    riseElement.innerHTML = `<p>Sunrise:</br>${new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}</p>`;
-    setElement.innerHTML = `<p>Sunset:</br>${new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}</p>`;
+    risesetElement.innerHTML = `<p>Sunrise:</br>${new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}</p></br> <p>Sunset:</br>${new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}</p>`;
+    // setElement.innerHTML = `<p>Sunset:</br>${new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}</p>`;
 }
